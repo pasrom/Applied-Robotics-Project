e614e9be8f3ab7c57ba489c4fa49d49bd8bb9cc0
pasrom <32713472+pasrom@users.noreply.github.com>
2017-12-29 21:37:36 +0100

    add setting ROS master IP

--- installScripts/9_add_bashrc.sh
+++ installScripts/9_add_bashrc.sh
@@ -3,3 +3,7 @@
 LINE='source ~/gopigo_ws/devel/setup.bash'
 FILE=~/.bashrc
 grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+LINE='. ~/workspace/Applied-Robotics-Project/installScripts/setRosMasterIP.sh'
+FILE=~/.bashrc
+grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
