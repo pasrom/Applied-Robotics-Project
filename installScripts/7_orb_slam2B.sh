cfb60c2a534f9d3beecae895184578b1d4c9ab00
pasrom <roman.passler@gmail.com>
2018-01-21 19:08:20 +0100

    changes...

--- installScripts/7_orb_slam2B.sh
+++ installScripts/7_orb_slam2B.sh
@@ -1,4 +1,6 @@
  #!/bin/bash
 
+cd ~/workspace/ORB_SLAM2
 chmod +x build_ros.sh
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/workspace/ORB_SLAM2/Examples/ROS
 bash ./build_ros.sh
--- installScripts/add_bashrc.sh
+++ installScripts/add_bashrc.sh
@@ -1,9 +1,24 @@
  #!/bin/bash
 
-LINE='source ~/gopigo_ws/devel/setup.bash'
+if [  "$USER" != "root" ]; then
+LINEA='source ~/gopigo_ws/devel/setup.bash'
 FILE=~/.bashrc
-grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+grep -qF "$LINEA" "$FILE" || echo "$LINEA" >> "$FILE"
 
-LINE='. ~/workspace/Applied-Robotics-Project/installScripts/setRosIp.sh'
+LINEB='. ~/workspace/Applied-Robotics-Project/installScripts/setRosIp.sh'
 FILE=~/.bashrc
-grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+grep -qF "$LINEB" "$FILE" || echo "$LINEB" >> "$FILE"
+fi
+
+
+if [  "$USER" = "root" ]; then
+FILE=~root/.bashrc
+LINEC='source /opt/ros/kinetic/setup.bash'
+grep -qF "$LINEC" "$FILE" || echo "$LINEC" >> "$FILE"
+LINED='. /home/roman/catkin_ws/devel/setup.bash'
+grep -qF "$LINED" "$FILE" || echo "$LINED" >> "$FILE"
+LINEA='source /home/roman/gopigo_ws/devel/setup.bash'
+grep -qF "$LINEA" "$FILE" || echo "$LINEA" >> "$FILE"
+LINEB='. /home/roman/workspace/Applied-Robotics-Project/installScripts/setRosIp.sh'
+grep -qF "$LINEB" "$FILE" || echo "$LINEB" >> "$FILE"
+fi
