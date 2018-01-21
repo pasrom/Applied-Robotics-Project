ab77783cea7a871cce089d781fc10bf234b7956e
pasrom <roman.passler@gmail.com>
2018-01-21 20:59:03 +0100

    added debug option

--- installScripts/install.sh
+++ installScripts/install.sh
@@ -4,10 +4,15 @@ echo Installing on $architecture
 
 ./1_installRos.sh
 echo "finished 1_installRos.sh"
-read -rsp $'Press enter to continue...\n'
+if [  "$1" = "debug" ]; then
+	read -rsp $'Press enter to continue...\n'
+fi
+
 ./2_environmentSetupRos.sh
 echo "finished 2_environmentSetupRos.sh"
-read -rsp $'Press enter to continue...\n'
+if [  "$1" = "debug" ]; then
+	read -rsp $'Press enter to continue...\n'
+fi
 
 #
 if [  "$architecture" = "armfh" ]; then
@@ -18,7 +23,9 @@ if [  "$architecture" = "armfh" ]; then
 	bash get_gopigo.sh
 	./add_bashrc.sh
 	echo "finished pigpio, gopigo and bashrc"
+	if [  "$1" = "debug" ]; then
 	read -rsp $'Press enter to continue...\n'
+	fi
 else
 #
 # this steps are only for the master / or other clients not raspy
@@ -27,23 +34,39 @@ else
 	. /opt/ros/kinetic/setup.bash
 	./add_bashrc.sh
 	echo "finished gopigo and bashrc"
-	read -rsp $'Press enter to continue...\n'
+	if [  "$1" = "debug" ]; then
+		read -rsp $'Press enter to continue...\n'
+	fi
+
 	./3_eigen.sh
 	echo "finished 3_eigen.sh"
-	read -rsp $'Press enter to continue...\n'
+	if [  "$1" = "debug" ]; then
+		read -rsp $'Press enter to continue...\n'
+	fi
+
 	./4_Pangolin.sh
 	echo "finished 4_Pangolin.sh"
-	read -rsp $'Press enter to continue...\n'
+	if [  "$1" = "debug" ]; then
+		read -rsp $'Press enter to continue...\n'
+	fi
+
 	./5_OpenCV.sh
 	echo "finished 5_OpenCV.sh"
-	read -rsp $'Press enter to continue...\n'
+	if [  "$1" = "debug" ]; then
+		read -rsp $'Press enter to continue...\n'
+	fi
+
 	./6_DBoW2_eigen_g2o_.sh
 	echo "finished 6_DBoW2_eigen_g2o_.sh"
-	read -rsp $'Press enter to continue...\n'
+	if [  "$1" = "debug" ]; then
+		read -rsp $'Press enter to continue...\n'
+	fi
+
 	./7_orb_slam2A.sh
 	source ~/.bashrc
 	./7_orb_slam2B.sh
-	#bash -c './7_orb_slam2.sh ; bash '
 	echo "finished 7_orb_slam2.sh"
-	read -rsp $'Press enter to continue...\n'
+	if [  "$1" = "debug" ]; then	
+		read -rsp $'Press enter to continue...\n'
+	fi
 fi
