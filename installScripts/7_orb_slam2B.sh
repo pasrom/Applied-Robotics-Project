c874ecb1980e043ce14c4ebe0b89edd488972dff
pasrom <roman.passler@gmail.com>
2018-01-22 11:28:20 +0100

    added symbolic links

--- installScripts/7_orb_slam2B.sh
+++ installScripts/7_orb_slam2B.sh
@@ -4,3 +4,7 @@ cd ~/workspace/ORB_SLAM2
 chmod +x build_ros.sh
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/workspace/ORB_SLAM2/Examples/ROS
 bash ./build_ros.sh
+
+# create symbolic link
+ln -s ~/workspace/ORB_SLAM2/Examples/Monocular/Logitech640x480.yaml ~/workspace/ORB_SLAM2/Examples/ROS/ORB_SLAM2/src/scripts/Logitech640x480.yaml
+ln -s ~/workspace/ORB_SLAM2/Vocabulary/ORBvoc.txt ~/workspace/ORB_SLAM2/Examples/ROS/ORB_SLAM2/src/scripts/ORBvoc.txt
