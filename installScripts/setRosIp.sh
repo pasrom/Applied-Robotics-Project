d6ad51ea520213daf156d7e3fa33e5318e951ebb
pasrom <roman.passler@gmail.com>
2018-01-23 14:41:17 +0100

    added different master ip, if we are in the edurom wireless network

--- installScripts/setRosIp.sh
+++ installScripts/setRosIp.sh
@@ -6,7 +6,12 @@ architecture="$(dpkg --print-architecture)"
 echo $architecture
 if [  "$architecture" = "armhf" ] || [ "$1" != "" ] ; then
 	echo raspy
-	MASTER_IP=192.168.0.107
+	if [ $IP = *"192.168."* ]; then
+    	MASTER_IP=192.168.0.107
+    else
+    	MASTER_IP=172.21.200.130
+    fi
+
 	export ROS_MASTER_URI=http://$MASTER_IP:11311
 	export ROS_IP=${IP::-1} #onw IP
 else
